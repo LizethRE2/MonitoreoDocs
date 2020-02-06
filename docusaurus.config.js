acd60c8ccd0a1bbf6314149980de8e2d21efa317
LizethRE2 <lizethre@ufps.edu.co>
2020-02-06 10:47:41 -0500

    actualización de configuración

---
+++ docusaurus.config.js
@@ -0,0 +1,118 @@
+module.exports = {
+  title: 'SISTEMA DE MONITOREO DE RED', // Title for your website.
+  tagline: 'UFPS Cúcuta & Campo Elíseos',
+  favicon: 'img/NMS.ico',
+  url: 'https://localhost', // Your website URL
+  //url: 'https://LizethRE2.github.io',
+  baseUrl: '/', // Base URL for your project */
+  //baseUrl: '/documentacion/',
+  organizationName: 'LizethRE2', // Usually your GitHub org/user name.
+  projectName: 'MonitoreoDoc', // Usually your repo name.
+  themeConfig: {
+    disableDarkMode: true, //Switch de modo Black
+    sidebarCollapsible: true, //Desplegable navegación
+    navbar: {
+      hideOnScroll: false, //Header oculto
+      title: 'Sistema de Monitoreo de Red',
+      logo: {
+        alt: 'CSI - NMS',
+        src: 'img/NMS.ico',
+      },
+      links: [
+        {
+          to: 'docs/inicio',
+          label: 'Documentación',
+          position: 'right'
+        },
+        {
+          href: 'https://github.com/LizethRE2/MonitoreoDocs',
+          label: 'Iniciar Sesión',
+          position: 'right'
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: '',
+          items: [
+            {
+              html: `
+              <div style="text-align: center;">
+                <a href="http://localhost:3000/documentacion" rel="noreferrer noopener" aria-label="CSI-UFPS">
+                  <img class="img-responsive" src="/img/ImgFooter.ico" alt="CSI - UFPS" style="width: 140px;"/>
+                  </a>
+              </div>
+              `,
+            },
+          ],
+        },
+        {
+          title: 'Documentación',
+          items: [
+            {
+              label: 'Instalación',
+              to: 'docs/introduccion',
+            },
+            {
+              label: 'Configuración',
+              to: 'docs/doc2',
+            },
+            {
+              label: 'Administración',
+              to: 'docs/doc3',
+            },
+          ],
+        },
+        {
+          title: 'Enlaces de Interés',
+          items: [
+            {
+              label: 'UFPS',
+              href: 'https://ww2.ufps.edu.co',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/LizethRE2/MonitoreoDocs',
+            },
+            {
+              label: 'Docusaurus',
+              href: 'https://v2.docusaurus.io',
+            },
+          ],
+        },
+        {
+          title: 'Contactos',
+          items: [
+            {
+              html: `
+              <address style="font-style: normal;">
+                      Avenida Gran Colombia No. 12E-96 Barrio Colsag, Edificio Aula Sur, Oficina 405 - C.S.I<br>
+                      San José de Cúcuta - Colombia<br>
+                      Teléfono: (057)(7) 5776655 Ext: 277<br>
+                      Correo electrónico: <a href="mailto:csi@ufps.edu.co" class="">csi@ufps.edu.co</a>
+                  </address>
+              `,
+            },
+          ],
+        },
+      ],
+      copyright: `© ${new Date().getFullYear()} C.S.I. Todos los derechos reservados.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/LizethRE2/MonitoreoDocs/edit/master/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ sidebars.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  docs: [
+    //{
+      //type: 'doc',
+      //id: 'inicio',
+    //},
+    //{
+      //type: 'category',
+      //label: 'Servidor',
+      //items: ['servidor', 'instalacionS'],
+    //},
+    //{
+      //type: 'category',
+      //label: 'Herramienta',
+      //items: ['acerca', 'doc2'],
+    //},
+    {
+      type: 'category',
+      label: 'SNMP',
+      items: ['doc1', 'doc2', 'doc3'],
+    },
+    //{
+      //type: 'category',
+      //label: 'Backup',
+      //items: ['backup', 'mdx', 'respaldo'],
+    //},
+  ],
+  //someSidebar: {
+    //NM: ['introduccion'],
+    //Servidor: ['servidor', 'instalacionS'],
+    //Herramienta: ['acerca', 'doc2'],
+    //SNMP: ['doc3'],
+    //Backup: ['backup', 'mdx'],
+    //Introducción2: [
+      //'introduccion',
+      //{
+        //type: 'category',
+        //label: 'Docs',
+        //items: ['doc1'],
+        //},
+    //],
+  //},
+};
